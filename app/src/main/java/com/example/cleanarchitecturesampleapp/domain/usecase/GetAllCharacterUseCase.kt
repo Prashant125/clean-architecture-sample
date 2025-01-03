12e4a7ac0c6e7db4a7ab38c824eb7f57c91a1688
Prashant Babule <pbabule@gmail.com>
2025-01-03 20:11:32 +0530

    added state to handle data

--- app/src/main/java/com/example/cleanarchitecturesampleapp/domain/usecase/GetAllCharacterUseCase.kt
+++ app/src/main/java/com/example/cleanarchitecturesampleapp/domain/usecase/GetAllCharacterUseCase.kt
@@ -1,10 +1,9 @@
 package com.example.cleanarchitecturesampleapp.domain.usecase
 
-import com.example.cleanarchitecturesampleapp.domain.model.Characters
 import com.example.cleanarchitecturesampleapp.domain.repository.CharacterRepository
 import javax.inject.Inject
 
-class getAllCharacterUseCase @Inject constructor(val repository: CharacterRepository) {
+class GetAllCharacterUseCase @Inject constructor(private val repository: CharacterRepository) {
 
     suspend operator fun invoke() = repository.getAllCharacters()
 }
